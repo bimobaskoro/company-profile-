2fb0a1346b06f4eb5e865d5483f1873da94884eb
bimobaskoro <bimoappbaskoro@gmail.com>
2024-05-03 21:18:45 +0700

    update teams

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -40,22 +40,24 @@ const Teams = async () => {
 
   return (
     <div>
-      <div className="mb-4">
-        <div className="text-black text-2xl font-bold flex justify-center items-center max-[320px]:mt-3 min-[375px]:mt-10 min-[425px]:mt-16 md:mt-20 lg:text-5xl xl:mt-32">
-          Our Teams
-        </div>
-        <div className="inline-flex items-center justify-center w-full">
-          <hr className="w-64 h-1 my-2 bg-gray-900 border-0 rounded dark:bg-gray-700" />
-          <div className="absolute px-4 -translate-x-1/2 bg-white left-1/2 dark:bg-gray-900">
-            <svg
-              className="w-4 h-4 text-gray-700 dark:text-gray-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 14"
-            >
-              <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-            </svg>
+      <div className="relative">
+        <div
+          className="bg-cover bg-center h-60 w-full"
+          style={{ backgroundImage: `url("/images/teams.jpg")` }}
+        >
+          <div className="absolute inset-0 bg-black opacity-70"></div>
+          <div className="absolute w-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+            <h1 className="text-3xl md:text-6xl lg:text-7xl font-bold mb-1">
+              Our Teams
+            </h1>
+            <div className="flex justify-center items-center w-auto">
+              <p className="text-sm md:text-xl lg:text-2xl">Home / </p>
+              <p className="text-sm md:text-xl lg:text-2xl">About / </p>
+              <p className="text-sm md:text-xl lg:text-2xl">Our Service / </p>
+              <p className=" text-slate-400 text-sm md:text-xl lg:text-2xl">
+                Our Teams
+              </p>
+            </div>
           </div>
         </div>
       </div>
